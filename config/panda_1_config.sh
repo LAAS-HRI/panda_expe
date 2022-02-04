63f2acfe8aed91ab1cdcacc9832c26dd48f96106
Yannickriou <yannick.riou@laas.fr>
2022-02-04 16:48:53 +0100

    added ROS related configuration for both panda to set the ROS_MASTER_URI and ROS_IP variable

---
+++ config/panda_1_config.sh
@@ -0,0 +1,3 @@
+export ROS_IP=140.93.16.149
+export ROS_MASTER_URI=http://140.93.16.216:11311
+export Franka_DIR=/home/yriou/PANDA/libfranka/build
---
+++ config/panda_2_config.sh
@@ -0,0 +1,4 @@
+export ROS_IP=140.93.16.216
+export ROS_HOSTNAME=$ROS_IP
+export ROS_MASTER_URI=http://140.93.16.216:11311
+export Franka_DIR=/home/yriou/PANDA/libfranka/build
